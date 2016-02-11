42e06c2b8921cdc8666705f64919dea865c048ef
ChrisspyB <christopher.bradley@live.co.uk>
2016-02-11 21:19:11 +0000

    Added ship placement

--- battleship.js
+++ battleship.js
@@ -2,76 +2,114 @@
 var canvas = document.getElementById('canvasBS');
 var ctx = canvas.getContext('2d'); 
 
+var rot_toggled = false; //
+
+var STATE = {
+	placement:0,
+	leftTurn:1,
+	rightTurn:2,
+	gameOver:3
+};
+var COLOR = {
+	water:'#aaaaff',
+	ship:'#aaaaaa',
+	hit:'#ffaaaa',
+	miss:'#ddddff'
+};
+var shipHeight = [5,4,3,3,2];
+
 var game = {
 	x:10,
 	y:10,
 	squareSize:32,
 	boardSep:32,
 	boardSqLen:10,
-	squareColor: '#aaaaff'
+	state:STATE.placement
 };
 game.boardLen = game.boardSqLen*game.squareSize;
 
 var mark = {
-	ship: 	1, 	//flag square has having a ship
-	shot: 	2, 	//flag square as being shot
-	interest:4 	//flag square as marked as interesting by player (RMB)
+	ship: 	1,
+	hit: 	2,
+	miss: 	4,
+	interest:8 	//flag square as marked as interesting by player (eg by RMB)
 };
-
 function GameManager(local){
 	/*
-	x,y 	= board origin
 	local 	= is this an offline game
 	*/
-	var separation = 20; // *magic number
 	this.boards = [	new Board(game.x,game.y),
 					new Board(game.x + game.boardLen + game.boardSep, game.y)];
 }
-
 GameManager.prototype.draw = function() {
-	
+	this.drawUI();
+	this.boards[0].draw();
+	this.boards[1].draw();
+};
+GameManager.prototype.drawUI = function() {
 };
 
 function Board(x,y){
 	this.x = x;
 	this.y = y;
+	this.ships = 0;
 	this.friendly = true; // Does the board belong to the local player
 	this.map = [];
-	for (var i=0; i++; i<game.boardSqLen){
-		for (var i=0; i<game.boardSqLen; i++ ){
-			this.map.append(0);
+	for (var i=0; i<game.boardSqLen; i++ ){
+		this.map.push([]);
+		for (var j=0; j<game.boardSqLen; j++ ){
+			this.map[i].push(0);
 		}
 	}
 }
-Board.prototype.addShip = function(x,y,h,hor) {
+Board.prototype.addShip = function(x,y) {
 	/*
 	x,y = grid position of top left of ship.
-	h 	= length of ship
-	hor = (optional) if true ship is placed horizontal. Vertical otherwise
 	*/
-	hor = hor || 0;
-	if(hor){
+	if (this.ships == shipHeight.length){return;}
+	var h   = shipHeight[this.ships]
+
+	if(rot_toggled){
+		if (x+h > game.boardSqLen){return;}
 		for (var i=x; i<x+h; i++){
-			this.map[i][y] = 1;
+			if (this.map[i][y] & mark.ship){return;}
+		}
+		for (var i=x; i<x+h; i++){
+			this.map[i][y] = mark.ship;
 		}
 	}
 	else{
+		if (y+h > game.boardSqLen){return;}
 		for (var i=y; i<y+h; i++){
-			this.map[x][i] = 1;
+			if (this.map[x][i] & mark.ship){return;}
 		}
+		for (var i=y; i<y+h; i++){
+			this.map[x][i] = mark.ship;
+		}		
 	}
+
+	this.ships+=1;
 };
 
 Board.prototype.draw = function() {
-	for (var x=this.x; x<this.x+game.boardLen; x+=game.squareSize){
-		for (var y=this.y; y<this.y+game.boardLen; y+=game.squareSize){
+	for (var i=0; i<game.boardSqLen; i++){
+		for (var j=0; j<game.boardSqLen; j++){
+
 			ctx.beginPath();
-			ctx.rect(x,y,game.squareSize,game.squareSize);
-			ctx.fillStyle = game.squareColor
-			// Render extra here...
+			ctx.rect(this.x+game.squareSize*i,this.y+game.squareSize*j,game.squareSize,game.squareSize);
+			if(this.map[i][j] & mark.hit) {	ctx.fillStyle = COLOR.hit; }
+			else { ctx.fillStyle = COLOR.water; }
 			ctx.fill();
 			ctx.stroke();
 			ctx.closePath();
+			if(this.map[i][j] & mark.ship) {
+				// draw ship ...
+				ctx.beginPath();
+				ctx.rect(this.x+game.squareSize*i,this.y+game.squareSize*j,game.squareSize,game.squareSize);
+				ctx.fillStyle = COLOR.ship;
+				ctx.fill();
+				ctx.closePath();
+			}
 		}
 	}
 };
@@ -80,23 +118,61 @@ Board.prototype.clickSquare = function(mouse_x,mouse_y) {
 		mouse_y < this.y || mouse_y > this.y + game.boardLen) { return; }
 	var i = Math.floor((mouse_x - this.x) / game.squareSize);
 	var j = Math.floor((mouse_y - this.y) / game.squareSize);
-};	
 
+	if (game.state == STATE.placement){
+		this.addShip(i,j)
+	}
+};	
 
 
 //Testing
 var gm = new GameManager(true);
-gm.boards[0].draw();
-gm.boards[1].draw();
+gm.draw();
+
 
 document.addEventListener('click',function(event){
-		var rect = canvas.getBoundingClientRect();
-		var mouse_x = event.clientX  - rect.left;
-		var mouse_y = event.clientY - rect.top;
-		if (mouse_x > game.x + game.boardLen){
-			gm.boards[1].clickSquare(mouse_x,mouse_y);
+	var rect = canvas.getBoundingClientRect();
+	var mouse_x = event.clientX  - rect.left;
+	var mouse_y = event.clientY - rect.top;
+	if (mouse_x > game.x + game.boardLen){
+		gm.boards[1].clickSquare(mouse_x,mouse_y);
+	}
+	else{
+		gm.boards[0].clickSquare(mouse_x,mouse_y);
+	}
+});
+
+function mouseMove (event) {
+	if (game.state!=STATE.placement){
+		document.removeEventListener('mousemove',mouseMove);
+		return;
+	}
+
+	var rect = canvas.getBoundingClientRect();
+	var mouse_x = event.clientX  - rect.left;
+	var mouse_y = event.clientY - rect.top;
+	ctx.clearRect(0,0,canvas.width,canvas.height);
+	gm.draw();
+	ctx.beginPath();
+	if (rot_toggled)
+		for (var i=0; i<shipHeight[gm.boards[0].ships]; i++) { 
+			ctx.rect(mouse_x+game.squareSize*i-game.squareSize/2,mouse_y,game.squareSize,game.squareSize);
 		}
-		else{
-			gm.boards[0].clickSquare(mouse_x,mouse_y);
+	else{
+		for (var i=0; i<shipHeight[gm.boards[0].ships]; i++) { 
+			ctx.rect(mouse_x-game.squareSize/2,mouse_y+game.squareSize*i,game.squareSize,game.squareSize);
 		}
-	});
+	}
+	ctx.fillStyle = COLOR.ship;
+	ctx.fill();
+	ctx.stroke();
+	ctx.closePath();
+}
+document.addEventListener('mousemove',mouseMove);
+
+document.addEventListener('keydown',function(event){
+	if (event.keyCode == 32){
+		rot_toggled = ~rot_toggled;
+		// mouseMove(event);
+	}
+});
